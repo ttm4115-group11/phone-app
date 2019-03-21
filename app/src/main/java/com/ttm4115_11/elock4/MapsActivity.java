c214371089021a188a1910f7e5d396b199ac8e30
haavard@vaage.com <haavard@vaage.com>
2019-03-21 09:50:28 +0100

    Change initial coords to Trondheim

--- app/src/main/java/com/ttm4115_11/elock4/MapsActivity.java
+++ app/src/main/java/com/ttm4115_11/elock4/MapsActivity.java
@@ -29,7 +29,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -38,9 +37,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Trondheim and move the camera
+        LatLng tTown = new LatLng(63.446827, 10.421906);
+        mMap.addMarker(new MarkerOptions().position(tTown).title("Marker in Trondheim"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tTown));
     }
 }
